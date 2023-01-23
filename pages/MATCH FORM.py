c852d03e1bb97c8ec74f8a7a135b2653aa37bbf7
Viktoriia <viktoria.telna@gmail.com>
2023-01-23 22:39:21 +0100

    Update MATCH FORM.py

--- pages/MATCH FORM.py
+++ pages/MATCH FORM.py
@@ -1,14 +1,14 @@
 from pages.base_page import BasePage
 class MatchForm (BasePage):
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[5]
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[18]/div
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[7]/div/div
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[15]/button/span[1]
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[3]/button[1]/span[1]
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[14]/div/label
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[6]
-    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]
-    Button_xpath = //*[@id="__next"]/div[1]/main
+    Button_xpath1 = //*[@id="__next"]/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]
+    Button_xpath2 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[5]
+    Button_xpath3 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[18]/div
+    Button_xpath4 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[7]/div/div
+    Button_xpath5 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[15]/button/span[1]
+    Button_xpath6 = //*[@id="__next"]/div[1]/main/div[2]/form/div[3]/button[1]/span[1]
+    Button_xpath7 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[14]/div/label
+    Button_xpath8 = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[6]
+    Button_xpath9 = //*[@id="__next"]/div[1]/main/div[2]
+    Button_xpath10 = //*[@id="__next"]/div[1]/main
 
 pass
