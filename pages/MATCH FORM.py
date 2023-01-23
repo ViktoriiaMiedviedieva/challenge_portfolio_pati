061d4c601c798c2c614240810d8c2e93596d7f32
Viktoriia <viktoria.telna@gmail.com>
2023-01-23 22:28:14 +0100

    Create MATCH FORM.py

---
+++ pages/MATCH FORM.py
@@ -0,0 +1,14 @@
+from pages.base_page import BasePage
+class MatchForm (BasePage):
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[5]
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[18]/div
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[7]/div/div
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[15]/button/span[1]
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[3]/button[1]/span[1]
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[14]/div/label
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[6]
+    Button_xpath = //*[@id="__next"]/div[1]/main/div[2]
+    Button_xpath = //*[@id="__next"]/div[1]/main
+
+pass
