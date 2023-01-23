d113244f93c063da660f93b01abec03f791dc7d5
Viktoriia <viktoria.telna@gmail.com>
2023-01-23 22:27:55 +0100

    Create dashboard.py

---
+++ pages/dashboard.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+
+class Dasboard(BasePage):
+   Button_xpath = "//*[@id="password"]"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[3]/div[2]/div/div/a/button"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[3]/div[1]"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[3]/div[3]"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div[1]"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div[3]"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[1]"
+   Button_xpath = "//*[@id="__next"]/div[1]/header/div"
+   Button_xpath = "//*[@id="__next"]/div[1]/div/div"
+   Button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div[3]/div/div[2]"
+
+ pass
