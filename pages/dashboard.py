b092b895fe57767150931bbc7b66953b0203bfdd
Viktoriia <viktoria.telna@gmail.com>
2023-01-24 22:49:13 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,15 +1,15 @@
 from pages.base_page import BasePage
 
 class Dasboard(BasePage):
-   Button_xpath1 = "//*[@id="password"]"
-   Button_xpath2 = "//*[@id="__next"]/div[1]/main/div[3]/div[2]/div/div/a/button"
-   Button_xpath3 = "//*[@id="__next"]/div[1]/main/div[3]/div[1]"
-   Button_xpath4 = "//*[@id="__next"]/div[1]/main/div[3]/div[3]"
-   Button_xpath5 = "//*[@id="__next"]/div[1]/main/div[2]/div[1]"
-   Button_xpath6 = "//*[@id="__next"]/div[1]/main/div[2]/div[3]"
-   Button_xpath7 = "//*[@id="__next"]/div[1]/main/div[1]"
-   Button_xpath8 = "//*[@id="__next"]/div[1]/header/div"
-   Button_xpath9 = "//*[@id="__next"]/div[1]/div/div"
-   Button_xpath10 = "//*[@id="__next"]/div[1]/main/div[2]/div[3]/div/div[2]"
+   Button_xpath1 = "//*[@id='password']"
+   Button_xpath2 = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/a/button"
+   Button_xpath3 = "//*[@id='__next']/div[1]/main/div[3]/div[1]"
+   Button_xpath4 = "//*[@id='__next']/div[1]/main/div[3]/div[3]"
+   Button_xpath5 = "//*[@id='__next']/div[1]/main/div[2]/div[1]"
+   Button_xpath6 = "//*[@id='__next']/div[1]/main/div[2]/div[3]"
+   Button_xpath7 = "//*[@id='__next']/div[1]/main/div[1]"
+   Button_xpath8 = "//*[@id='__next']/div[1]/header/div"
+   Button_xpath9 = "//*[@id='__next']/div[1]/div/div"
+   Button_xpath10 = "//*[@id='__next']/div[1]/main/div[2]/div[3]/div/div[2]"
 
  pass
